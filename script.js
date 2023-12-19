999bf46f403d3675119cb7795cdb98d31315ad79
gabriellyandelina <140662912+gabriellyandelina@users.noreply.github.com>
2023-12-19 16:41:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,9 +28,11 @@
            
            description = "Cuidado! Você está com sobrepeso!";
         }  else if (imc >= 30 && imc < 35) {
-            description = "Cuidado! Você está com obesidade grau I!";
+            description = "Cuidado! Você está com obesidade grau I.";
+        } else if (imc >= 35 && imc < 39,9) {
+            description = "Cuidado! Você está com obesidade grau II.";
         } else {
-            description = "Cuidado! Você está com obesidade grau II!";
+            description = "Cuidado! Você está com obesidade grau III.";
         }
 
         value.textContent = imc.replace('.',',');
