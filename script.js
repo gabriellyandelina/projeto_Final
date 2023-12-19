dc42698e5cde6ca5f6393213384a9750581e3cdc
gabriellyandelina <140662912+gabriellyandelina@users.noreply.github.com>
2023-12-19 13:33:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,10 @@
     btn.addEventListener("click", function (event) {
         event.preventDefault();
         
-        const weight = document.getElementById('weight').value;
-        const height = document.getElementById('height').value;
+        const weight = document.getElementById('peso').value;
+        const height = document.getElementById('altura').value;
 
-        const bmi = (weight/(height * height)).toFixed(2);
+        const imc = (peso/(altura * altura)).toFixed(2);
 
 
         const value = document.getElementById('value');
@@ -20,20 +20,20 @@
 
         document.getElementById('infos').classList.remove('hidden');
 
-        if (bmi<18.5){
+        if (imc<18.5){
             description ="Cuidado! Você está abaixo do peso"
-        } else if (bmi >= 18.5 && bmi < 25) {
+        } else if (imc >= 18.5 && bmi < 25) {
             description = "Você está no peso ideal!";
-        }  else if (bmi >= 25 && bmi < 30) {
+        }  else if (imc >= 25 && bmi < 30) {
            
            description = "Cuidado! Você está com sobrepeso!";
-        }  else if (bmi >= 30 && bmi < 35) {
+        }  else if (imc >= 30 && bmi < 35) {
             description = "Cuidado! Você está com obesidade moderada!";
         } else {
             description = "Cuidado! Você está com obesidade morbida!";
         }
 
-        value.textContent = bmi.replace('.',',');
+        value.textContent = imc.replace('.',',');
         document.getElementById("description").textContent = description;
         
     });
